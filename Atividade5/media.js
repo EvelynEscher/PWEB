e449ab21ec494425943bd40122fc50323f0a0370
EvelynEscher <fernandes.evelynmartins@gmail.com>
2020-09-23 21:57:20 -0300

    atividade 5 e exemplos de Javascript

---
+++ Atividade5/media.js
@@ -0,0 +1,7 @@
+const nota1 = prompt("insira nota1");
+const nota2 = prompt("insira nota2");
+const nota3 = prompt("Insira nota3");
+
+const media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
+
+alert("media do aluno: " + media);
---
+++ Atividade5/operacao.js
@@ -0,0 +1,12 @@
+const numero2 = prompt("Insira o primeiro numero");
+const numero2 = prompt("Insira o segundo numero");
+
+alert = ("O valor da soma é "+parseFloat(numero1)+parseFloat(numero2));
+
+alert = ("O valor da subtração é "+parseFloat(numero1)-parseFloat(numero2));
+
+alert = ("O valor do pruduto é "+parseFloat(numero1)*parseFloat(numero2));
+
+alert = ("O valor da divisão é "+parseFloat(numero1)/parseFloat(numero2));
+
+alert = ("O valor do resto da divisão é "+parseFloat(numero1)%parseFloat(numero2));
